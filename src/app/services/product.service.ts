c701f083b4a9490076f97debd095fa7f3ca734b9
Nada Ossama <118224738+NadaOssama2001@users.noreply.github.com>
2024-11-13 01:28:34 +0200

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,8 +13,10 @@ export class ProductService {
   productColors: string[] = [];
   //private apiUrl = 'http://localhost:5204/api/products';
   //private imgmvcurl = 'http://localhost:5269/';
-   private apiUrl = 'http://2b-sohag.runasp.net/api/products';
-   private imgmvcurl = 'http://2begypt.runasp.net/';
+   // private apiUrl = 'http://2b-sohag.runasp.net/api/products';
+   // private imgmvcurl = 'http://2begypt.runasp.net/';
+   private apiUrl = 'https://2beapi.runasp.net/api/Products';
+   private imgmvcurl = 'https://2begypt.runasp.net/';
   // private imgmvcurl = 'http://localhost:29510/';
   constructor(private httpclient: HttpClient) {}
 
